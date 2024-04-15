33cf3587ed70dd5be20cc1486a616c5c1cd7f209
ViktorStankov <viktorstankov1@gmail.com>
2024-04-16 00:15:48 +0300

    refactor: Make class static

--- helpers/data_extractor.py
+++ helpers/data_extractor.py
@@ -2,18 +2,14 @@ from typing import List
 
 
 class DataExtractor:
-    def __init__(self, path: str):
-        self.path = path
-
-    def extract_data(self) -> List[List[float]]:
+    @staticmethod
+    def extract_data(path: str) -> List[List[float]]:
         output = []
         try:
-            with open(self.path, 'r') as file:
+            with open(path, 'r') as file:
                 lines = file.readlines()
                 for line in lines:
                     output.append([float(x) for x in line.strip().split(',')])
-        except FileNotFoundError:
-            raise FileNotFoundError('File not found.')
         except ValueError:
             raise ValueError('Invalid data in input file.')
 
